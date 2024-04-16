9b0e4bb5c391307042191b12af85dc55f5b561f4
Alexander Holmes <108624793+AlexanderHolmes0@users.noreply.github.com>
2024-04-16 18:12:35 -0400

    udpateA5..

--- A5/lm_mc.R
+++ A5/lm_mc.R
@@ -1,7 +1,7 @@
 suppressMessages(library(arrow,quietly = TRUE))
 suppressMessages(library(tidymodels,verbose = FALSE,warn.conflicts = FALSE,quietly = TRUE))
 library(future)
-plan(cluster, workers = as.numeric(commandArgs(TRUE)[2]))
+plan(multisession, workers = as.numeric(commandArgs(TRUE)[2]))
 set.seed(seed = 123, "L'Ecuyer-CMRG")  
 
 #nc = as.numeric(commandArgs(TRUE)[2])   
